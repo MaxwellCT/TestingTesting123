d32e48406438ffb7ad0fe62c83aa0c933954be7f
Centrous <maxwellchanetekimang@gmail.com>
2025-04-15 12:12:21 -0400

    made test to make sure item added to list and test to make sure item is added to collection.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,7 +41,10 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:1.9.0")
+    testImplementation("org.testng:testng:6.9.6")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 }
--- app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
+++ app/src/test/java/com/example/testingtesting123/UniqueCollectionTest.kt
@@ -12,18 +12,30 @@ class UniqueCollectionTest {
         collection = UniqueCollection()
     }
 
-
     // TODO 1: Write a test to ensure items can be added to the collection
     @Test
     fun addAnItem() {
-
+        val item = Item("item1")
+        collection.addItem(item)
+        assert(collection.size() == 1) { "Item was not added" }
+        assert(collection.get(0).name == "item1") { "Item name does not match" }
     }
 
     // TODO 2: Write a test to ensure that only unique items can be added to the collection
     // Uniqueness is determined by the Item.name property, which is set via the constructor
     @Test
     fun addUniqueItem() {
+        val item1 = Item("item1")
+        val item2 = Item("item1")
+        val item3 = Item("item2")
 
+        collection.addItem(item1)
+        collection.addItem(item2)
+        collection.addItem(item3)
+
+        assert(collection.size() == 2) { "Duplicate item was added" }
+        assert(collection.get(0).name == "item1") { "First item name mismatch" }
+        assert(collection.get(1).name == "item2") { "Second item name mismatch" }
     }
 
     // Test Driven Development (TDD) test - complete specified function so that this test passes
